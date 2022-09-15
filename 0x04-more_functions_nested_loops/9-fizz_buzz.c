0a96c5863b5f945a4902ca22c844abaf1ec4aa75
HDV22 <heidi.devilliers060@gmail.com>
2022-09-15 21:53:49 +0200

    9 fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,29 +14,29 @@ int main(void)
 
 	while (num++ < 100)
 	{
-	if ((num % 3 == 0) && (num % 5 == 0))
-	{
-		printf("FizzBuzz ");
-	}
-	else if ((num % 3) == 0)
-	{
-		printf("Fizz ");
-	}
-	else if ((num % 5) == 0)
-	{
-		if (num != 100)
+		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("Buzz ");
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
 		}
 		else
 		{
-			printf("Buzz");
+			printf("%d ", num);
 		}
-	}
-	else
-	{
-		printf("%d ", num);
-	}
 	}
 	printf("\n");
 
