dfeeaf17c0348c0821fd5a5a3d7658c566cb2278
HDV22 <heidi.devilliers060@gmail.com>
2022-09-21 19:46:47 +0200

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * puts2 - prints every other character
- * @str: string
- *
- * Return: nothing
+ * cap_string - function that capitalize first character of a word
+ * @str: string to capitalize
+ * Return:returns the capitalized string
  */
-
-void puts2(char *str)
+char *cap_string(char *str)
 {
-	int i = 0;
-
-	while (*(str + i) != '\0')
-	{
-		if (i % 2 == 0)
-			putchar(*(str + i));
-		i++;
-	}
-	putchar(10);
+int index = 0;
+while (str[++index])
+{
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}')
+str[index] -= 32;
+}
+return (str);
 }
