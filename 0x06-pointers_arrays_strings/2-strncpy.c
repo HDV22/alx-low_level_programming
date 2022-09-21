9154cb192d5ac75a31c91a5b6616cb7e9f09d501
HDV22 <heidi.devilliers060@gmail.com>
2022-09-21 19:35:35 +0200

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
 /**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns length as integer;
+ * _strncpy - C function that copies a string, including the
+ * @dest: buffer storing the string copy
+ * @src:the source string
+ * @n:max nummber of byte copied
+ * Return: returns
  */
-
-int _strlen(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
 }
