b0d5a10a4bc03ff6a76e3c0d7ce5090b1702486a
HDV22 <heidi.devilliers060@gmail.com>
2022-09-22 19:04:01 +0200

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * void prototypes()
- * int prototypes()
- * char prototypes()
- */
+* void prototypes()
+* int prototypes()
+* char prototypes()
+*/
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
