5943d390ed03c858b43b006090d8b0b42a1a9a77
HDV22 <heidi.devilliers060@gmail.com>
2022-10-13 23:12:33 +0200

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -13,7 +13,7 @@ char *t;
 void (*f)(va_list);
 }print_t;
 
-int _putchar(char c);
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
